818cf799c5d83301890ceef25d58a5999ba40c86
Anar Hasanov <anar.hasanov79@gmail.com>
2024-06-16 13:15:28 +0400

    add api path for product endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://qyk12uqv60.execute-api.us-east-1.amazonaws.com/prod",
+  product: "https://2r92h0rs80.execute-api.us-east-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
